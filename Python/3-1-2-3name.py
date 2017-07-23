6d79a935896e0a010cb09b1e8c76672d59f941e8
Jeason <jeason.liang@gmail.com>
2017-07-24 02:28:01 +0800

    3-1-2-3name

---
+++ Python/3-1-2-3name.py
@@ -0,0 +1,16 @@
+#3-1 姓名：将一些朋友的姓名存储在一个列表中，并将其命名为 names。依次访问该列表中的每个元素，从而将每个朋友的姓名都打印出来。
+name = ['jeassca','lancer','jeason','ailice']
+print(name[-1].title(),)
+print(name[0].title())
+print(name[1].title())
+print(name[2].title())
+#3-2 问候语：继续使用练习 3-1 中的列表，但不打印每个朋友的姓名，而为每人打印一条消息。每条消息都包含相同的问候语，但抬头为相应朋友的姓名。
+print(name[-1].title()+'! Welcome to learn Python !')
+print(name[0].title()+'! Welcome to learn Python !')
+print(name[1].title()+'! Welcome to learn Python !')
+print(name[2].title()+'! Welcome to learn Python !')
+#3-3 自己的列表：想想你喜欢的通勤方式，如骑摩托车或开汽车，并创建一个包含多种通勤方式的列表。根据该列表打印一系列有关这些通勤方式的宣言，如“I would like to own a Honda motorcycle”。
+
+print('I would like to own a',moto[-1].title(),'motorcycle!')
+
+
